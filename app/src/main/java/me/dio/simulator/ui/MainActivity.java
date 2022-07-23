9d0fc86b61b34021a3c4d8d6ec7ca83918851d05
suellenmuniz <su09muniz@hotmail.com>
2022-07-22 21:44:37 -0300

    Finalização do Faribase e ajustes do App

--- app/src/main/java/me/dio/simulator/ui/MainActivity.java
+++ app/src/main/java/me/dio/simulator/ui/MainActivity.java
@@ -2,19 +2,16 @@ package me.dio.simulator.ui;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
-import android.icu.text.UnicodeSetIterator;
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.service.autofill.FieldClassification;
-import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.snackbar.Snackbar;
-
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -22,7 +19,6 @@ import me.dio.simulator.R;
 import me.dio.simulator.data.MatchesAPI;
 import me.dio.simulator.databinding.ActivityMainBinding;
 import me.dio.simulator.domain.Match;
-import me.dio.simulator.domain.Team;
 import me.dio.simulator.ui.adapter.MatchesAdapter;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -34,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private MatchesAPI matchesApi;
-    private MatchesAdapter matchesAdapter;
+    private MatchesAdapter matchesAdapter = new MatchesAdapter(Collections.emptyList());
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
@@ -49,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
         setupMatchesRefFresh();
         setupFloatingActionButton();
     }
-
     private void setupHttpClient() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://suellenmuniz.github.io/matches-simulator-api/")
@@ -58,58 +53,53 @@ public class MainActivity extends AppCompatActivity {
 
         matchesApi = retrofit.create(MatchesAPI.class);
     }
-
-
     private void setupMatchesList() {
         binding.rvMatches.setHasFixedSize(true);
         binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvMatches.setAdapter(matchesAdapter);
         findMatchesFromApi();
     }
     private void setupMatchesRefFresh() {
         binding.srlMatches.setOnRefreshListener(this::findMatchesFromApi);
     }
-
     private void setupFloatingActionButton() {
         binding.fabSimulate.setOnClickListener(view -> {
             view.animate().rotationBy(360).setDuration(500).setListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
-                   Random random = new Random();
-                    for (int i = 0; i <matchesAdapter.getItemCount(); i++) {
-                    Match match = matchesAdapter.getMatches().get(i);
-                    match.getHomeTeam().setScore(random.nextInt(match.getHomeTeam().getStars() + 1));
-                    match.getAwayTeam().setScore(random.nextInt(match.getAwayTeam().getStars() + 1));
-                    matchesAdapter.notifyItemChanged(i);
-                    }
-                }
+                    Random random = new Random();
+                   for (int i = 0; i < matchesAdapter.getItemCount(); i++) {
+                        Match match = matchesAdapter.getMatches().get(i);
+                        match.getHomeTeam().setScore(random.nextInt(match.getHomeTeam().getStars() + 1));
+                        match.getAwayTeam().setScore(random.nextInt(match.getAwayTeam().getStars() + 1));
+                        matchesAdapter.notifyItemChanged(i);
+                   }
+               }
             });
-           });
-        }
-
+        });
+    }
     private void showErrorMessage() {
         Snackbar.make(binding.fabSimulate, R.string.error_api, Snackbar.LENGTH_LONG).show();
     }
-
     private void findMatchesFromApi() {
-        binding.srlMatches.setRefreshing(true);
-        matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
-            @Override
-            public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
-                if (response.isSuccessful()) {
-                    List<Match> matches = response.body();
-                    Log.i("SIMULATOR", "Deu tudo certo! Voltaram partidas:" + matches.size());
-                    matchesAdapter = new MatchesAdapter(matches);
-                    binding.rvMatches.setAdapter(matchesAdapter);
-                } else {
-                    showErrorMessage();
-                }
-                binding.srlMatches.setRefreshing(false);
-            }
-
-            @Override
-            public void onFailure(Call<List<Match>> call, Throwable t) {
+    binding.srlMatches.setRefreshing(true);
+    matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
+        @Override
+        public void onResponse(@NonNull Call<List<Match>> call, @NonNull Response<List<Match>> response) {
+            if (response.isSuccessful()) {
+                List<Match> matches = response.body();
+                matchesAdapter = new MatchesAdapter(matches);
+                binding.rvMatches.setAdapter(matchesAdapter);
+            } else {
                 showErrorMessage();
             }
+            binding.srlMatches.setRefreshing(false);
+        }
 
-        });
-    }
+        @Override
+        public void onFailure(@NonNull Call<List<Match>> call, @NonNull Throwable t) {
+            showErrorMessage();
+            binding.srlMatches.setRefreshing(false);
+        }
+    });}
+}
